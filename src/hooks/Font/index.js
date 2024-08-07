1e3f3e03bd1288793c42bda938b6bb8851d11663
elloysabigas <161051191+elloysabigas@users.noreply.github.com>
2024-08-07 13:00:09 +0000

    [feat] Expo-font with Contex

--- src/hooks/Font/index.js
+++ src/hooks/Font/index.js
@@ -1,15 +1,26 @@
 import { useFonts } from "expo-font";
 import { createContext, useContext } from "react";
+import { ActivityIndicator, View, Text} from "react-native";
 
 const FontContex = createContext({})
 
 export function FontProvider({children}) {
 
     const [loaded, error] = useFonts({
-        regular: require("../../assets/")
+        regular: require("../../assets/fonts/Montserrat-Regular.ttf"),
+        bold: require("../../assets/fonts/Montserrat-Bold.ttf"),
     });
 
-    return <FontContex.Provider value={{}}>{children}</FontContex.Provider>;
+    if (!loaded && !error) {
+        return(
+            <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+                <Text style={{fontSize: 28, marginTop: 15}}>Carregando as fontes</Text>
+                <ActivityIndicator size='large' color='0000ff' />
+            </View>
+        );
+      }
+
+    return <FontContex.Provider value={{loaded}}>{children}</FontContex.Provider>;
 }
 
 export function useFont() {
