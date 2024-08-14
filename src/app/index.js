de5a0b52a8cb1cded95e76d2255d944f3937657c
elloysabigas <161051191+elloysabigas@users.noreply.github.com>
2024-08-14 14:08:28 +0000

    [feat] Auth Context

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { useAuth } from '../hooks/Auth';
+import { router } from 'expo-router';
 
 export default function App() {
   const {signIn, signOut} = useAuth ();
+
+  const handleEntrarSuper = async () => {
+    try{
+      await signIn({ email: "super@email.com", password: "Super123!" })
+      router.replace("/");
+    } catch (error) {
+      console.log(e)
+    }
+  }
   
   return (
     <View style={styles.container}>
       <Text style={styles.title}> Minhas Plantas</Text>
-      <Button title="Signin Super" onPress={()=>signIn({email: "super@email.com", password: "Super123!"})} />
+      <Button title="Signin Super" onPress={handleEntrarSuper} />
       <Button title="Signin Adm" onPress={()=>signIn({email: "adm@email.com", password: "Adm123!"})} />
       <Button title="Signin User" onPress={()=>signIn({email: "user@email.com", password: "User123!"})} />
       <Button title="Signout" onPress={()=>signOut()} />
