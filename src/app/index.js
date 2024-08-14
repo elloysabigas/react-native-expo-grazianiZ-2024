5ba4dd18b9c474fab121f120208a462da0ddc624
elloysabigas <161051191+elloysabigas@users.noreply.github.com>
2024-08-14 21:31:50 +0000

    [feat] Auth Context

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { BackHandler, Button, StyleSheet, Text, View } from 'react-native';
 import { useAuth } from '../hooks/Auth';
 import { router } from 'expo-router';
 
@@ -17,11 +17,12 @@ export default function App() {
   
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> Minhas Plantas</Text>
+      <Text style={styles.title}>Aplicativo Pronto para usar</Text>
       <Button title="Signin Super" onPress={handleEntrarSuper} />
       <Button title="Signin Adm" onPress={()=>signIn({email: "adm@email.com", password: "Adm123!"})} />
       <Button title="Signin User" onPress={()=>signIn({email: "user@email.com", password: "User123!"})} />
-      <Button title="Signout" onPress={()=>signOut()} />
+      <Button title="Sobre" onPress={() => router.push("/about") } />
+      <Button title="Sair do aplicativo" onPress={() => BackHandler.exitApp()} />
       <StatusBar style="auto" />
     </View>
   );
@@ -33,6 +34,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 15,
   },
   title:{
     fontFamily:'bold',
