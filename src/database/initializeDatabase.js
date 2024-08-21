3d050f9aaa25d988ab33d79f836a7e66d206bccb
elloysabigas <161051191+elloysabigas@users.noreply.github.com>
2024-08-21 14:08:34 +0000

    [feat] Database

---
+++ src/database/initializeDatabase.js
@@ -0,0 +1,24 @@
+export async function initializeDatabase(database){
+    try {
+        await database.execAsync(`
+            DROP TABLE IF EXISTS users;
+
+            CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            email TEXT NULL UNIQUE,
+            senha TEXT NOT NULL DEFAULT 'A123456a!',
+            role TEXT NOT NULL DEFAULT 'USER',
+            created_at DATE DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATE,
+            );
+
+            INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Super', 'super@email.com', 'A123456!', 'SUPER');
+            INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Admin', 'admin@email.com', 'A123456!', 'ADMIN');
+            INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('User', 'user@email.com', 'A123456!', 'USER');
+
+            `);
+    } catch (error){
+    console.log(error);
+    }
+}
--- src/hooks/Auth/index.js
+++ src/hooks/Auth/index.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-
+import { authUser } from "../../database/useUsersDatabase"
 
 const AuthContext = createContext({});
 
@@ -19,30 +19,22 @@ export function AuthProvider({children}){
     });
 
     const signIn = async ({email, password}) => {
-        if (email === "super@email.com" && password==="Super123!"){
-                setUser({
-                    autenticated: true,
-                    user: {id: 1, name: "Super Usuário", email}, 
-                    role: Role.SUPER,
-                 });
-        } else if (email === "adm@email.com" && password==="Adm123!"){
-            setUser({
-                autenticated: true,
-                user: {id: 2, name: "Administrador", email}, 
-                role: Role.ADM,
-             });
-        } else if (email === "user@email.com" && password==="User123!"){
-            setUser({
-                autenticated: true,
-                user: {id: 3, name: "Usuário Comum", email}, 
-                role: Role.USER,
-             });
-        } else {
+        const response = await authUser({ email, password });
+
+        if (!response) {
             setUser({
                 autenticated: false,
                 user: null, 
                 role: null,
              });
+        }
+
+        setUser({
+            autenticated: true,
+            user: response, 
+            role: response.role,
+
+        
         }
     };
     const signOut = async () => {
