8be03f645e678b644bb2df6df280e3004ab6706c
elloysabigas <161051191+elloysabigas@users.noreply.github.com>
2024-10-23 13:27:59 +0000

    [feat] sobre

--- src/app/(protected)/sobresamambaia.js
+++ src/app/(protected)/sobresamambaia.js
@@ -1,60 +1,158 @@
-import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
 import React, { useState } from 'react'; 
 
 export default function Flores() {
   const [showMessage1, setShowMessage1] = useState(false);
   const [showMessage2, setShowMessage2] = useState(false);
+  const [showMessage3, setShowMessage3] = useState(false);
 
   const handlePress1 = () => {
     setShowMessage1(!showMessage1);
-    setShowMessage2(false); // Fecha o lembrete do segundo botão se estiver aberto
+    setShowMessage2(false); 
+    setShowMessage3(false);
   };
 
   const handlePress2 = () => {
     setShowMessage2(!showMessage2);
-    setShowMessage1(false); // Fecha o lembrete do primeiro botão se estiver aberto
+    setShowMessage1(false); 
+    setShowMessage3(false);
+  };
+
+  const handlePress3 = () => {
+    setShowMessage3(!showMessage3);
+    setShowMessage1(false); 
+    setShowMessage2(false);
   };
 
   const closeMessage = () => {
     setShowMessage1(false);
     setShowMessage2(false);
+    setShowMessage3(false);
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Image style={styles.image} source={require('../../../src/assets/images/samambaia.jpg')} />
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <Image style={styles.image} source={require('../../../src/assets/images/samambaia.jpg')} />
 
-      <Text style={styles.title}>Samambaias</Text>
-      <Text>Samambaias ● Cyrtomium</Text>
+        <Text style={styles.title}>Samambaias</Text>
+        <Text style={styles.title2}>Samambaias ● Cyrtomium</Text>
 
-      {/* Contêiner para os botões */}
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.smallButton} onPress={handlePress1}>
-          <Text style={styles.buttonText}>Fácil de Cuidar</Text>
-        </TouchableOpacity>
+        <View style={styles.buttonContainer}>
+          <View style={styles.column}>
+            <TouchableOpacity style={styles.smallButton} onPress={handlePress1}>
+              <Text style={styles.buttonText}>Fácil de Cuidar</Text>
+            </TouchableOpacity>
 
-        <TouchableOpacity style={styles.smallButton2} onPress={handlePress2}>
-          <Text style={styles.buttonText}>Média</Text>
-        </TouchableOpacity>
-      </View>
-
-      {/* Lembrete do primeiro botão */}
-      {showMessage1 && (
-        <TouchableOpacity style={styles.overlay} onPress={closeMessage} activeOpacity={1}>
-          <View style={styles.messageContainer}>
-            <Text style={styles.messageText}>A planta é fácil de cuidar e boa para iniciantes</Text>
+            <TouchableOpacity style={styles.smallButton} onPress={handlePress3}>
+              <Text style={styles.buttonText}>Purificadora de ar</Text>
+            </TouchableOpacity>
           </View>
-        </TouchableOpacity>
-      )}
 
-      {/* Lembrete do segundo botão */}
-      {showMessage2 && (
-        <TouchableOpacity style={styles.overlay} onPress={closeMessage} activeOpacity={1}>
-          <View style={styles.messageContainer}>
-            <Text style={styles.messageText}>As plantas crescem de 30 cm a 1,2 m de altura</Text>
+          <TouchableOpacity style={[styles.smallButton, styles.marginLeft]} onPress={handlePress2}>
+            <Text style={styles.buttonText}>Média</Text>
+          </TouchableOpacity>
+        </View>
+
+        {showMessage1 && (
+          <TouchableOpacity style={styles.overlay} onPress={closeMessage} activeOpacity={1}>
+            <View style={styles.messageContainer}>
+              <Text style={styles.messageText}>A planta é fácil de cuidar e boa para iniciantes</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+
+        {showMessage2 && (
+          <TouchableOpacity style={styles.overlay} onPress={closeMessage} activeOpacity={1}>
+            <View style={styles.messageContainer}>
+              <Text style={styles.messageText}>As plantas crescem de 30 cm a 1,2 m de altura</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+
+        {showMessage3 && (
+          <TouchableOpacity style={styles.overlay} onPress={closeMessage} activeOpacity={1}>
+            <View style={styles.messageContainer}>
+              <Text style={styles.messageText}>
+                Elas podem ajudar a melhorar a qualidade do ar em ambientes internos ao filtrar poluentes e toxinas.
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+
+        <Text style={styles.aboutTitle}>Sobre</Text>
+        <Text style={styles.title2}>
+          As samambaias são plantas vasculares antigas que se reproduzem por esporos, em vez de sementes ou flores. Elas têm frondes (folhas longas) que se desenrolam em espiral ao crescer e possuem rizomas (caules subterrâneos) que ajudam na propagação. Preferem ambientes úmidos e sombreados, sendo comuns em florestas tropicais e usadas como plantas ornamentais. Algumas espécies são conhecidas por melhorar a qualidade do ar em ambientes internos.
+        </Text>
+
+        <View style={styles.imageContainer}>
+        <Text style={styles.aboutTitle}>Detalhes</Text>
+          <View style={styles.imageItem}>
+            <Image style={styles.itemImage} source={require('../../../src/assets/images/29.jpg')} />
+            <View style={styles.textContainer}>
+              <Text style={styles.itemText}>Temperatura</Text>
+              <Text style={styles.itemText2}>25°C - 30°C</Text>
+            </View>
           </View>
-        </TouchableOpacity>
-      )}
+          <View style={styles.imageItem}>
+            <Image style={styles.itemImage} source={require('../../../src/assets/images/30.jpg')} />
+            <View style={styles.textContainer}>
+              <Text style={styles.itemText}>Luz solar</Text>
+              <Text style={styles.itemText2}>Sombra Parcial</Text>
+            </View>
+          </View>
+          <View style={styles.imageItem}>
+            <Image style={styles.itemImage} source={require('../../../src/assets/images/31.jpg')} />
+            <View style={styles.textContainer}>
+              <Text style={styles.itemText}>Água</Text>
+              <Text style={styles.itemText2}>Uma vez por semana</Text>
+            </View>
+          </View>
+          <View style={styles.imageItem}>
+            <Image style={styles.itemImage} source={require('../../../src/assets/images/32.jpg')} />
+            <View style={styles.textContainer}>
+              <Text style={styles.itemText}>Transplante</Text>
+              <Text style={styles.itemText2}>A cada 12 a 24 meses</Text>
+            </View>
+          </View>
+          <View style={styles.imageItem}>
+            <Image style={styles.itemImage} source={require('../../../src/assets/images/33.jpg')} />
+            <View style={styles.textContainer}>
+              <Text style={styles.itemText}>Fertilização</Text>
+              <Text style={styles.itemText2}>Aplique Fertilização Líquido {'\n'}
+                a cada 2 a 4 meses</Text>
+            </View>
+          </View>
+          <View style={styles.imageItem}>
+            <Image style={styles.itemImage} source={require('../../../src/assets/images/34.jpg')} />
+            <View style={styles.textContainer}>
+              <Text style={styles.itemText}>Pragas</Text>
+              <Text style={styles.itemText2}>Cochonilhas</Text>
+            </View>
+          </View>
+        </View>
+
+          <Text style={styles.aboutTitle}>Problemas Comuns</Text>
+        <View style={styles.imageContainer}>
+        
+          <View style={styles.imageItem}>
+            <Image style={styles.itemImage} source={require('../../../src/assets/images/38.png')} />
+            <View style={styles.textContainer}>
+              <Text style={styles.itemText}>Podridão Apical</Text>
+              <Text style={styles.itemText2}>25°C - 30°C</Text>
+            </View>
+          </View>
+          <View style={styles.imageItem}>
+            <Image style={styles.itemImage} source={require('../../../src/assets/images/39.png')} />
+            <View style={styles.textContainer}>
+              <Text style={styles.itemText}>Oídio</Text>
+              <Text style={styles.itemText2}>Sombra Parcial</Text>
+            </View>
+          
+          </View>
+        </View>
+
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -62,8 +160,10 @@ export default function Flores() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
     backgroundColor: '#f9f9f9',
+  },
+  scrollViewContent: {
+    padding: 16,
   },
   image: {
     width: '100%',
@@ -71,28 +171,37 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    fontWeight: 'regular',
+    fontWeight: 'bold',
     marginTop: 20,
+    color: '#005f56',
+  },
+  title2: {
+    fontSize: 16,
+    color: '#1b1b1b',
+    textAlign: 'justify',
+    padding: 10,
+    fontFamily: 'regular',
   },
   buttonContainer: {
-    flexDirection: 'row', // Alinha os botões horizontalmente
-    justifyContent: 'space-between', // Espaçamento igual entre os botões
-    marginTop: 50, // Diminui a distância acima dos botões
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginTop: 10,
+  },
+  column: {
+    flexDirection: 'column',
   },
   smallButton: {
-    paddingVertical: 10, // Aumenta a altura do botão
+    paddingVertical: 10,
     paddingHorizontal: 10,
-    backgroundColor: '#dfdfdf',
+    backgroundColor: '#f4f5f9',
     borderRadius: 5,
     alignItems: 'center',
-    width: 130, // Reduz a largura dos botões
-    marginHorizontal: 5, // Adiciona espaço horizontal entre os botões
+    width: 130,
+    marginVertical: 5,
   },
-
-  smallButton2:{
-    marginLeft:-20
+  marginLeft: {
+    marginLeft: 20,
   },
-
   buttonText: {
     color: '#005f56',
     fontSize: 10,
@@ -109,8 +218,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   messageContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    width: 150,
     backgroundColor: '#fff',
     padding: 10,
     borderRadius: 10,
@@ -123,5 +231,45 @@ const styles = StyleSheet.create({
   messageText: {
     fontSize: 16,
     color: '#333',
+  },
+  aboutTitle: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginTop: 20,
+    color: '#005F56',
+  },
+  imageContainer: {
+    marginTop: 20,
+    backgroundColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 15,
+    height: 520,
+  },
+  imageItem: {
+    paddingTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  itemImage: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+    borderRadius: 15,
+  },
+  textContainer: {
+    flexDirection: 'column', // Stack texts vertically
+    justifyContent: 'center',
+  },
+  itemText: {
+    fontSize: 14,
+    color: '#009B86',
+    lineHeight: 20,
+    fontFamily: 'bold',
+  },
+  itemText2: {
+    fontSize: 12,
+    color: '#777',
+    lineHeight: 18,
   },
 });
