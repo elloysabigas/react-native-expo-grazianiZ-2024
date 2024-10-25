9b84198108ae7c18495bd4083ccbcaeae7abe890
elloysabigas <161051191+elloysabigas@users.noreply.github.com>
2024-10-25 15:02:40 +0000

    [feat] finalizandosamambaia

--- src/app/(protected)/sobresamambaia.js
+++ src/app/(protected)/sobresamambaia.js
@@ -1,10 +1,12 @@
 import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
 import React, { useState } from 'react'; 
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Flores() {
   const [showMessage1, setShowMessage1] = useState(false);
   const [showMessage2, setShowMessage2] = useState(false);
   const [showMessage3, setShowMessage3] = useState(false);
+  const [showProblems, setShowProblems] = useState(false);
 
   const handlePress1 = () => {
     setShowMessage1(!showMessage1);
@@ -30,11 +32,15 @@ export default function Flores() {
     setShowMessage3(false);
   };
 
+  const toggleProblems = () => {
+    setShowProblems(!showProblems);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
         <Image style={styles.image} source={require('../../../src/assets/images/samambaia.jpg')} />
-
+        
         <Text style={styles.title}>Samambaias</Text>
         <Text style={styles.title2}>Samambaias ● Cyrtomium</Text>
 
@@ -49,7 +55,7 @@ export default function Flores() {
             </TouchableOpacity>
           </View>
 
-          <TouchableOpacity style={[styles.smallButton, styles.marginLeft]} onPress={handlePress2}>
+          <TouchableOpacity style={[styles.smallButton2, styles.marginLeft2]} onPress={handlePress2}>
             <Text style={styles.buttonText}>Média</Text>
           </TouchableOpacity>
         </View>
@@ -86,7 +92,7 @@ export default function Flores() {
         </Text>
 
         <View style={styles.imageContainer}>
-        <Text style={styles.aboutTitle}>Detalhes</Text>
+          <Text style={styles.aboutTitle}>Detalhes</Text>
           <View style={styles.imageItem}>
             <Image style={styles.itemImage} source={require('../../../src/assets/images/29.jpg')} />
             <View style={styles.textContainer}>
@@ -119,8 +125,7 @@ export default function Flores() {
             <Image style={styles.itemImage} source={require('../../../src/assets/images/33.jpg')} />
             <View style={styles.textContainer}>
               <Text style={styles.itemText}>Fertilização</Text>
-              <Text style={styles.itemText2}>Aplique Fertilização Líquido {'\n'}
-                a cada 2 a 4 meses</Text>
+              <Text style={styles.itemText2}>Aplique Fertilização Líquido {'\n'} a cada 2 a 4 meses</Text>
             </View>
           </View>
           <View style={styles.imageItem}>
@@ -132,23 +137,39 @@ export default function Flores() {
           </View>
         </View>
 
-          <Text style={styles.aboutTitle}>Problemas Comuns</Text>
-        <View style={styles.imageContainer}>
-        
+        <Text style={styles.aboutTitle}>Problemas Comuns</Text>
+        <View style={styles.imageContainer2}>
           <View style={styles.imageItem}>
-            <Image style={styles.itemImage} source={require('../../../src/assets/images/38.png')} />
+            <Image style={styles.itemImage} source={require('../../../src/assets/images/40.png')} />
             <View style={styles.textContainer}>
-              <Text style={styles.itemText}>Podridão Apical</Text>
-              <Text style={styles.itemText2}>25°C - 30°C</Text>
-            </View>
-          </View>
-          <View style={styles.imageItem}>
-            <Image style={styles.itemImage} source={require('../../../src/assets/images/39.png')} />
-            <View style={styles.textContainer}>
-              <Text style={styles.itemText}>Oídio</Text>
+              <Text style={styles.itemText}>Amarelecimento</Text>
               <Text style={styles.itemText2}>Sombra Parcial</Text>
             </View>
-          
+            <TouchableOpacity onPress={toggleProblems} style={styles.iconButton}>
+              <Icon name={showProblems ? "chevron-up" : "chevron-down"} size={20} color="#005f56" style={{marginLeft:50}} />
+            </TouchableOpacity>
+          </View>
+          {showProblems && (
+            <View style={styles.problemDetails}>
+              <Text style={styles.problemText}>O amarelecimento das folhas das samambaias pode ser um sinal de que a planta não está nas condições ideais. Esse fenômeno pode ocorrer por diversos motivos, como excesso de luz, rega inadequada, umidade baixa, deficiência nutricional ou a presença de pragas e doenças. Para resolver o problema, é importante observar as condições em que a samambaia está sendo cultivada e fazer os ajustes necessários.
+
+Para cuidar adequadamente de samambaias, siga algumas diretrizes básicas. Primeiro, coloque a planta em um local com luz indireta, evitando a exposição ao sol direto, que pode queimar as folhas. Mantenha o solo levemente úmido, regando quando a parte superior começar a secar, mas evite encharcar para prevenir raízes podres. Samambaias também apreciam um ambiente úmido, então você pode borrifar água nas folhas ou usar um umidificador para aumentar a umidade.
+
+Quanto à temperatura, mantenha a planta em um ambiente entre 18°C e 24°C, evitando correntes de ar frio. Durante a primavera e o verão, fertilize uma vez por mês com um fertilizante balanceado, diluído pela metade. Não esqueça de podar folhas secas ou amareladas para estimular o crescimento saudável. Por fim, se a planta estiver crescendo muito e as raízes estiverem apertadas, considere transplantá-la para um vaso maior a cada 1-2 anos.
+
+Com esses cuidados e ajustes, suas samambaias devem prosperar e exibir folhas verdes e saudáveis.</Text>
+            </View>
+          )}
+        </View>
+
+        <View style={styles.imageContainer2}>
+          <Text style={styles.aboutTitle}>Curiosidades</Text>
+          <View style={styles.imageItem}>
+            <Image style={styles.itemImage} source={require('../../../src/assets/images/41.jpg')} />
+            <View style={styles.textContainer}>
+              <Text style={styles.itemText}>Temperatura</Text>
+              <Text style={styles.itemText2}>Aplique Fertilização Líquido {'\n'} a cada 2 a 4 meses</Text>
+            </View>
           </View>
         </View>
 
@@ -198,8 +219,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: 130,
     marginVertical: 5,
+    height:40,
   },
   marginLeft: {
+    marginLeft: 20,
+  },
+  smallButton2: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    backgroundColor: '#f4f5f9',
+    borderRadius: 5,
+    alignItems: 'center',
+    width: 130,
+    marginVertical: 5,
+    height: 40, // Ajuste a altura aqui
+  },
+
+  marginLeft2: {
     marginLeft: 20,
   },
   buttonText: {
@@ -243,7 +279,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#f2f2f2',
     padding: 10,
     borderRadius: 15,
-    height: 520,
+  },
+  imageContainer2: {
+    marginTop: 20,
+    backgroundColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 15,
+    // Removido height para ajuste automático
   },
   imageItem: {
     paddingTop: 10,
@@ -258,7 +300,7 @@ const styles = StyleSheet.create({
     borderRadius: 15,
   },
   textContainer: {
-    flexDirection: 'column', // Stack texts vertically
+    flexDirection: 'column',
     justifyContent: 'center',
   },
   itemText: {
@@ -271,5 +313,22 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: '#777',
     lineHeight: 18,
+  },
+  iconButton: {
+    marginLeft: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  problemDetails: {
+    marginTop: 5,
+    padding: 10,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 10,
+    // Removido height para ajuste automático
+  },
+  problemText: {
+    fontSize: 12,
+    color: '#777',
+    textAlign: 'justify',
   },
 });
